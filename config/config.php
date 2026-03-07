9f40349bac0f28e69968b0d860aaefa63b5f7974
DibyenduCode <khaskeldibyendu22@gmail.com>
2026-03-07 20:15:47 +0530

    Folder structure

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+
+define("BASE_URL","http://localhost/cert");
+
+define("INSTITUTE_PREFIX","BPLF");
+define("INSTITUTE_CODE","00000001");
+define("STATE_CODE","WB");
+
+date_default_timezone_set("Asia/Kolkata");
